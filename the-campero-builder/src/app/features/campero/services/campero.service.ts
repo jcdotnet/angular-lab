9b491eb421aa7655e0d842de95aa182e6565f96b
JC <jcdotnet@hotmail.com>
2026-05-05 12:34:47 +0200

    feat: persist campero order

--- the-campero-builder/src/app/features/campero/services/campero.service.ts
+++ the-campero-builder/src/app/features/campero/services/campero.service.ts
@@ -1,17 +1,19 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { CamperoOrder } from '../models/campero.model';
 import { INGREDIENTS } from '../data/ingredients';
+
+const LOCAL_STORAGE_KEY = 'campero_order';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CamperoService {
-  order = signal<CamperoOrder | null>(null);
+  order = signal<CamperoOrder | null>(this.loadFromStorage());
 
   calculatePrice(ingredient: string, extras: string[], hasPromo: boolean): number {
 
-    const CAMPERO_BASICO = 3.50;
-    
+    const CAMPERO_BASICO = 3.50; // TO-DO: to import from external file 
+
     const ingredientPrice = INGREDIENTS.find(i => i.id === ingredient)?.price || 0;
     const extrasPrice = (extras || []).reduce((acc, id) => {
       const extra = INGREDIENTS.find(e => e.id === id);
@@ -22,11 +24,31 @@ export class CamperoService {
     return hasPromo ? subtotal * 0.8 : subtotal;
   }
 
-  setOrder(order: CamperoOrder) {
-    this.order.set(order);
+  constructor() {
+    effect(() => {
+      if (this.order()) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.order()));
+      } else {
+        localStorage.removeItem(LOCAL_STORAGE_KEY);
+      }
+    });
+  }
+
+  loadFromStorage(): CamperoOrder | null {
+    try {
+      const fromStorage = localStorage.getItem(LOCAL_STORAGE_KEY);
+      return fromStorage ? JSON.parse(fromStorage) : null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 
   resetOrder() {
     this.order.set(null);
+  }
+
+  setOrder(order: CamperoOrder) {
+    this.order.set(order);
   }
 }
